b8c95e42ffd93d59ebb0dc86a72f4771706e350c
MasterTinka <znamenochick@gmail.com>
2021-04-05 17:10:58 +0600

    Исправлен установщик

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-su
+if [ $USER != "root" ]
+then
+echo "Please, run this as super user"
+exit
+fi
 
 directory = $(pwd)
 
 qmake-qt5
 make 
 
-mkdir /home/$USER/.services
-cd /home/$USER/.services
-
-cp $directory/DaemonCooler ./DaemonCooler
+mv ./DaemonCooler /bin/DaemonCooler
+cd /bin
 
 echo -e "#!/bin/bash\n$(pwd)/DaemonCooler" > DaemonCooler.sh
 
@@ -17,7 +19,7 @@ chmod +x DaemonCooler.sh
 
 cd /etc/systemd/system 
 
-echo -e "[Unit]\nDescription = Cooler control\n\n[Service]\nRemainAfterExit=true\nExecStart=/home/$USER/.services/DaemonCooler.sh\nType=oneshot\n\n[Install]\nWantedBy=multi-user.target\n" > DaemonCooler.service
+echo -e "[Unit]\nDescription = Cooler control\n\n[Service]\nRemainAfterExit=true\nExecStart=/bin/DaemonCooler.sh\nType=oneshot\n\n[Install]\nWantedBy=multi-user.target\n" > DaemonCooler.service
 
 cd /etc
 echo -e "# Lines must be written in one piece\npin=4\nmax_temp=50\ninterval=10\nworking_time=600\n" > ./CoolerDaemon.conf
